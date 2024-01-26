accd35a6286a5997cd28977ef677480defbf861f
simjae97 <tlawo2232@gmail.com>
2024-01-26 13:37:39 +0900

    20240125

--- src/day18/controller/BoardController.java
+++ src/day18/controller/BoardController.java
@@ -2,6 +2,9 @@ package day18.controller;
 
 import day18.model.dao.BoardDao;
 import day18.model.dto.BoardDto;
+import day18.model.dto.CategoryDto;
+
+import java.util.ArrayList;
 
 public class BoardController {
     private BoardController(){}
@@ -11,6 +14,16 @@ public class BoardController {
         return boardController;
     }
 
+    public int boardWrite(BoardDto boardDto){
+        int result = BoardDao.getInstance().boardWrite(boardDto);
+        return result;
+    }
+
+    public ArrayList<CategoryDto> cateList(){
+        ArrayList<CategoryDto> result = new ArrayList<>();
+        result = BoardDao.getInstance().cateList();
+        return result;
+    }
     public BoardDto view(int num){ return BoardDao.getInstance().view(num);}
     public String findId(int num){ return BoardDao.getInstance().findId(num);}
     public String findCategory(int num){ return BoardDao.getInstance().findCategory(num);}
--- src/day18/view/BoardView.java
+++ src/day18/view/BoardView.java
@@ -3,7 +3,9 @@ package day18.view;
 import day18.controller.BoardController;
 import day18.controller.MemberController;
 import day18.model.dto.BoardDto;
+import day18.model.dto.CategoryDto;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BoardView {
@@ -24,7 +26,23 @@ public class BoardView {
             System.out.println("1.글쓰기 2.글보기 3.로그아웃");
             System.out.print("입력>");int ch= scanner.nextInt();
             if(ch==1){
-
+                ArrayList<CategoryDto> categoryDtos = new ArrayList<>();
+                categoryDtos = BoardController.getInstance().cateList();
+                for(CategoryDto i : categoryDtos){  // 카테고리DB에서 categoryDto들을 가져온 배열을 하나씩 출력해서 보여주기
+                    System.out.printf("%d.%s ",i.getCno(),i.getCname());
+                }
+//                System.out.println("1.자바 2.C언어"); // 샘플
+                System.out.print("카테고리 선택 > "); int category = scanner.nextInt();
+                scanner.nextLine();
+                System.out.print("글 제목 : "); String boardtitle = scanner.nextLine();
+                System.out.print("글 내용 : "); String boardcontents = scanner.nextLine();
+                // 입력받은 값 4개 dto에 담아서 컨트롤러로 전달
+                BoardDto boardDto = new BoardDto( category, MemberController.getInstance().getLoginMno(), boardtitle, boardcontents );
+                if(BoardController.getInstance().boardWrite(boardDto)==0){
+                    System.out.println("글쓰기 성공");
+                } else if (BoardController.getInstance().boardWrite(boardDto)==1) {
+                    System.out.println("글쓰기 실패");
+                }
             }
             else if (ch==2){
                 System.out.println("볼 번호 입력");
