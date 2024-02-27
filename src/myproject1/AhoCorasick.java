d4973f474845cec4b5a6c4157bf1f4b76a55da38
새ë¡tlawo22 <tlawo33@naver.com>
2024-02-27 14:14:52 +0900

    20240227아호코라식

--- src/myproject1/AhoCorasick.java
+++ src/myproject1/AhoCorasick.java
@@ -5,15 +5,15 @@ package myproject1;
 import java.util.*;
 
 public class AhoCorasick {
-    private TrieNode root;
+    private TrieNode root; //맨처음노드
 
     public AhoCorasick(List<String> patterns) {
-        root = new TrieNode();
-        buildTrie(patterns);
+        root = new TrieNode(); //부모노드 일단 만들고
+        buildTrie(patterns); //빌드로 트리와 실패상태 구현
         buildFailTransitions();
     }
 
-    private void buildTrie(List<String> patterns) {
+    private void buildTrie(List<String> patterns) { //받아온 리스트 패턴을 그대로 대입해줘서 Trie구조 생성
         for (String pattern : patterns) {
             TrieNode node = root;
             for (char c : pattern.toCharArray()) {
@@ -25,7 +25,7 @@ public class AhoCorasick {
     }
 
     private void buildFailTransitions() {
-        Queue<TrieNode> queue = new LinkedList<>();
+        Queue<TrieNode> queue = new LinkedList<>(); //실패상태는 queue구조 , KMP와 유사
         for (TrieNode child : root.children.values()) {
             queue.add(child);
             child.fail = root;
@@ -48,10 +48,10 @@ public class AhoCorasick {
 
 
     public List<String> search(String text) {
-        List<String> results = new ArrayList<>();
-        TrieNode currentState = root;
-        for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
+        List<String> results = new ArrayList<>(); //결과값 받아와줄 리설트 생성
+        TrieNode currentState = root;   //현재 위치 = 맨처음은 부모
+        for (int i = 0; i < text.length(); i++) {   //받아온 문자열반복문 돌리기
+            char c = text.charAt(i);    //trie구조이므로 char형태로
             while (currentState != null && !currentState.children.containsKey(c)) {
                 currentState = currentState.fail;
             }
--- src/myproject1/TrieNode.java
+++ src/myproject1/TrieNode.java
@@ -8,10 +8,10 @@ import java.util.Map;
 class TrieNode {
     Map<Character, TrieNode> children;
     TrieNode fail;
-    List<String> output;
+    List<String> output; //search후 결과값 반환할수있는 필드 생성해둠
 
     TrieNode() {
-        children = new HashMap<>();
+        children = new HashMap<>(); //결국 Trie는 맵 구조로 이루어진 트리형태
         output = new ArrayList<>();
     }
 }
